e0d62e3cf328dba84fffdb566531b28cdd1042e4
Hiro <sathish999499@gmail.com>
2022-01-29 19:27:33 +0530

    intial

---
+++ src/test/java/MavenDemo.java
@@ -0,0 +1,20 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MavenDemo {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http:/www.google.com");
+        String title = driver.getTitle();
+        WebElement Search = driver.findElement(By.name("q"));
+        Search.sendKeys("Messi"+ Keys.ENTER);
+        System.out.println(title);
+        driver.quit();
+    }
+}
